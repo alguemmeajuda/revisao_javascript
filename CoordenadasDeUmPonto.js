c9a9029cdcfdeecd91350a614e09a52c77db6383
alguemmeajuda <124810553+alguemmeajuda@users.noreply.github.com>
2023-12-07 18:24:10 -0300

    Update CoordenadasDeUmPonto.js

--- CoordenadasDeUmPonto.js
+++ CoordenadasDeUmPonto.js
@@ -1,10 +1,19 @@
-function media(){
-    var x,a,b,c;
-    a=8.0*2
-    b=9*0*3
-    c=10.0*5
+function ponto(){
+    var x,y,p;
+    var Q1,Q2,Q3,Q4;
+    x=4.5;
+    p=-2.2;
+    
+    if (x>0){
+        p=Q1||Q4;
+}else{
+        p=Q2||Q3;
+    }
 
-    x=(a=b=c)/10;
-    alert(x);
+    if (y>0){
+        p=Q2||Q1;
+    }else{
+        p=Q3||Q4;
+    }
 
-}
+alert(p);
